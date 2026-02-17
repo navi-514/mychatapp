74bb0372bcfb90d3622e25c1d801d3ee7bdeeb1e
navi-514 <118039735+navi-514@users.noreply.github.com>
2026-02-17 11:36:03 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
+
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -13,6 +18,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+
+//deployment ready
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 
 app.listen(PORT, () => {
