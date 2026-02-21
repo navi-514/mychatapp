a8ef6659d042d30d08f4914bcdaae702d3c9565c
navi-514 <118039735+navi-514@users.noreply.github.com>
2026-02-21 15:31:38 +0530

    login and  logout from front end

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,10 +2,12 @@ import {create} from 'zustand';
 import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
 
+
 export const useAuthStore = create((set,get) => ({
    authUser: null,
    isCheckingAuth: true,
    isSigningUp: false,
+   isLoggingIn: false,
 
    checkAuth: async () =>{
     
@@ -36,7 +38,37 @@ export const useAuthStore = create((set,get) => ({
     finally{
         set({ isSigningUp: false });
     }
-   }
+   },
+
+   login : async(data) =>{
+    set({ isLoggingIn: true });
+    try{
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+
+        toast.success("Account created successfully");
+    }
+    catch(error){
+        toast.error("Error during signup:", error.response.data.message);
+    }
+    finally{
+        set({ isLoggingIn: false });
+    }
+   },
+
+    logout : async() =>{
+    
+    try{
+      await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+    }
+    catch(error){
+       toast.error("Error during logout:", error.response.data.message);
+    }
+   
+   },
+
 
  
 }))
